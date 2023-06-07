f616467aadd388c76a32bbcdc2960b160aaf6be0
AmbujGuptaConeio <ambuj@coneio.com>
2023-06-08 04:17:10 +0530

    init

---
+++ ambujgupta/src/main/java/com/socgen/ambujgupta/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.socgen.ambujgupta.controller;
+
+import com.socgen.ambujgupta.model.Employee;
+import com.socgen.ambujgupta.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+@CrossOrigin("*")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
---
+++ ambujgupta/src/test/java/com/socgen/ambujgupta/AmbujguptaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socgen.ambujgupta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbujguptaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
