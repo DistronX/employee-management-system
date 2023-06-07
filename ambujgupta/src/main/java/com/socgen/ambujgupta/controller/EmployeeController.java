7f863d36ca095842eb3a86a637f47fd951fff546
AmbujGuptaConeio <ambuj@coneio.com>
2023-06-08 04:43:12 +0530

    Add findById for Employee

--- ambujgupta/src/main/java/com/socgen/ambujgupta/controller/EmployeeController.java
+++ ambujgupta/src/main/java/com/socgen/ambujgupta/controller/EmployeeController.java
@@ -1,8 +1,11 @@
 package com.socgen.ambujgupta.controller;
 
+import com.socgen.ambujgupta.exception.ResourceNotFoundException;
 import com.socgen.ambujgupta.model.Employee;
 import com.socgen.ambujgupta.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,5 +25,13 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee with id " + id + " does ont exist."));
+
+        return ResponseEntity.ok(employee);
     }
 }
